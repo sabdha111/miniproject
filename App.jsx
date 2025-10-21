c8c337c14fdaba0a2ce76dc4b2313a23238d231e
sabdha111 <sabdhachithali@gmail.com>
2025-10-21 21:40:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <main style={{padding: '1rem'}}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
